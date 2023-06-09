5984147733b0c3e5c86da73858acbfb6a7761698
Norman-w <85535885@qq.com>
2023-06-09 11:34:11 +0800

    支持了倒计时和刷新

--- lib/barcode_auto_refresh_shower.dart
+++ lib/barcode_auto_refresh_shower.dart
@@ -3,6 +3,7 @@
 //region 导入需要的库
 import 'dart:async';
 
+import 'package:camp_launcher/enum.dart';
 import 'package:flutter/material.dart';
 //使用dio库
 import 'package:dio/dio.dart';
@@ -68,7 +69,8 @@ class _BarcodeAutoRefreshShowerState extends State<BarcodeAutoRefreshShower> {
         setState(() {
           _barcodeImage = Image.network(response.data['qrCodeUrl']);
           //将返回的二维码有效时间赋值给_remainingSeconds
-          _remainingSeconds = response.data['expire_seconds'];
+          // _remainingSeconds = response.data['expire_seconds'];
+          _remainingSeconds = 5;
         });
       }
     }).catchError((error) {
@@ -87,45 +89,148 @@ class _BarcodeAutoRefreshShowerState extends State<BarcodeAutoRefreshShower> {
   //region 重写构建方法
   @override
   Widget build(BuildContext context) {
-    //region 定义需要的组件
-    //定义一个组件列表
-    List<Widget> children = [];
-    //定义一个loading组件
-    var loading = const Center(
-      child: CircularProgressIndicator(),
-    );
-    //定义一个失败组件
-    var failed = const Center(
-      child: Text('获取二维码失败'),
-    );
-    //定义一个倒计时组件
-    var countdown = Center(
-      child: Text('$_remainingSeconds'),
-    );
-    //endregion
-    //region 组装组件列表
-    //如果_barcodeImage不为空,说明获取到了二维码,将二维码添加到组件列表中
+    //进入程序以后开始获取二维码
+    //获取二维码的超时时间为Constant里面定义的
+    //获取完了以后展示在页面上,显示倒计时
+    //倒计时时间到了以后继续重复获取二维码,并且获取完了以后展示二维码和倒计时
+    //如果获取二维码失败了,显示失败,提示点击刷新
+    EnumUIState uiState = EnumUIState.loadingBarcode;
+    //如果_barcodeImage不为空,说明获取二维码成功了
     if (_barcodeImage != null) {
-      children.add(_barcodeImage!);
+      //如果剩余时间大于0,说明二维码还没有过期
+      if (_remainingSeconds > 0) {
+        //设置uiState为显示二维码
+        uiState = EnumUIState.barcodeShowing;
+      } else {
+        //否则,设置uiState为显示二维码过期
+        uiState = EnumUIState.barcodeExpired;
+      }
+    } else {
     }
-    //如果_remainingSeconds大于0,说明还有剩余时间,将倒计时组件添加到组件列表中
-    if (_remainingSeconds > 0) {
-      children.add(countdown);
+    //根据uiState的值,返回不同的组件
+    switch (uiState) {
+    //如果是加载二维码中
+      case EnumUIState.loadingBarcode:
+      //返回一个加载中的组件
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+    //如果是二维码显示并倒计时中等待扫码
+      case EnumUIState.barcodeShowing:
+      //返回一个带有二维码图片和倒计时的组件
+        return Column(
+          children: [
+            //二维码图片
+            _barcodeImage!,
+            //倒计时
+            Text('剩余时间:$_remainingSeconds'),
+          ],
+        );
+    //如果是二维码失效等待点击重新获取
+      case EnumUIState.barcodeExpired:
+      //二维码图片组件
+      var barcodeImage = _barcodeImage!;
+      //遮罩层组件
+      var barcodeImageMask = Container(
+        //设置遮罩层的宽度和高度
+        width: barcodeImage.width,
+        height: barcodeImage.height,
+        //设置遮罩层的圆角
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: Colors.black54,
+        ),
+        //设置遮罩层的内边距
+        padding: const EdgeInsets.all(10),
+        //遮罩层里面的内容
+        child: Column(
+          children: [
+            //遮罩层里面的上面添加二维码已过期的提示
+            const Text('二维码已过期,请点击刷新'),
+            //遮罩层里面的下面放一个刷新按钮,点击按钮可以刷新二维码
+            ElevatedButton(
+              onPressed: () {
+                //重新获取二维码
+                _getQRCode();
+                //开始倒计时
+                _startCountdown();
+              },
+              child: const Text('刷新'),
+            ),
+          ],
+        ),
+      );
+      //二维码上面添加遮罩层,圆角并且有padding
+      return Stack(
+        children: [
+          //二维码图片
+          barcodeImage,
+          //遮罩层
+          barcodeImageMask,
+        ],
+      );
+      //遮罩层里面的上面添加二维码已过期的提示
+      //遮罩层里面的下面放一个刷新按钮,点击按钮可以刷新二维码
+    //如果是二维码扫码成功正在登录
+      case EnumUIState.barcodeScanned:
+      //返回一个带有二维码图片和倒计时的组件
+        return Column(
+          children: [
+            //二维码图片
+            _barcodeImage!,
+            //倒计时
+            Text('二维码已过期,请点击刷新'),
+          ],
+        );
+    //如果是二维码扫码失败提示中
+      case EnumUIState.barcodeScanFailed:
+      //返回一个带有二维码图片和倒计时的组件
+        return Column(
+          children: [
+            //二维码图片
+            _barcodeImage!,
+            //倒计时
+            Text('二维码已过期,请点击刷新'),
+          ],
+        );
+    //如果是游戏启动中
+      case EnumUIState.gameLaunching:
+      //返回一个带有二维码图片和倒计时的组件
+        return Column(
+          children: [
+            //二维码图片
+            _barcodeImage!,
+            //倒计时
+            Text('二维码已过期,请点击刷新'),
+          ],
+        );
+    // //如果是游戏启动失败
+    // case EnumUIState.gameLaunchFailed:
+    //   //返回一个带有二维码图片和倒计时的组件
+    //   return Column(
+    //     children: [
+    //       //二维码图片
+    //       _barcodeImage!,
+    //       //倒计时
+    //       Text('二维码已过期,请点击刷新'),
+    //     ],
+    //   );
+    // //如果是游戏启动成功
+    // case EnumUIState.gameLaunched:
+    //   //返回一个带有二维码图片和倒计时的组件
+    //   return Column(
+    //     children: [
+    //       //二维码图片
+    //       _barcodeImage!,
+    //       //倒计时
+    //       Text('二维码已过期,请点击刷新'),
+    //     ],
+    //   );
+      default:
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
     }
-    //如果_barcodeImage为空,说明还没有获取到二维码,将loading组件添加到组件列表中
-    if (_barcodeImage == null) {
-      children.add(loading);
-    }
-    //如果_remainingSeconds等于0,说明二维码已经失效,将失败组件添加到组件列表中
-    if (_remainingSeconds == 0) {
-      children.add(failed);
-    }
-    //endregion
-    //region 返回组件
-    return Column(
-      children: children,
-    );
-    //endregion
   }
   //endregion
 }
