3e82b7e0c7a9b26055c421cd719c79d9afc42218
Norman-w <85535885@qq.com>
2023-06-09 10:34:19 +0800

    开发环境设置

--- lib/constant.dart
+++ lib/constant.dart
@@ -4,12 +4,12 @@ class  Constant {
   //正式环境服务器地址
   static const String _serverUrlProd = "https://camp.norman.wang";
   //测试环境服务器地址
-  static const String _serverUrlTest = "https://camp.norman.wang";
+  static const String _serverUrlTest = "https://localhost:7028";
   //当前服务器地址,如果是正式环境,则使用正式环境地址,否则使用测试环境地址
   static const String serverUrl = _serverUrlTest;
 
   //api路径
-  static const String serverUrlApi = "$serverUrl/api";
+  static const String serverUrlApi = "$serverUrl/common";
 
   //心跳包上报路径
   static const String serverUrlHeartbeat = "$serverUrlApi/heartbeat";
