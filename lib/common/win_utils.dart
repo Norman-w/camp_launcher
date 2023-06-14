a17d003d29b8586decd91db51112daaf810cc183
norman <85535885@qq.com>
2023-06-14 14:06:46 +0800

    使用dart.io配合wmic命令检查当前系统中运行的所有进程信息.

--- lib/common/win_utils.dart
+++ lib/common/win_utils.dart
@@ -1,6 +1,7 @@
-import 'dart:ui';
-import 'package:win32/win32.dart';
-
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
+import '../sdk/model/ProcessInfo.dart';
 import 'package:window_manager/window_manager.dart';
 
 Future<void> initWindow () async {
@@ -15,3 +16,90 @@ Future<void> initWindow () async {
       }
   );
 }
+
+///使用Wmic命令获取进程信息
+Future<List<ProcessInfo>> getProcessesByWmic() async {
+  //如果当前运行系统不是windows,则直接返回空数组
+  if (!Platform.isWindows) {
+    //log
+    debugPrint('当前系统不是windows,无法使用wmic命令获取进程信息');
+    return [];
+  }
+  List<ProcessInfo> ret = [];
+  try {
+    var processResult = await Process.run(
+        'wmic', ['process', 'get', 'processid,caption,executablepath']);
+    if (processResult.exitCode == 0) {
+      var output = processResult.stdout;
+      var lines = LineSplitter.split(output);
+      var processPaths = lines.skip(1)
+          .where((line) => line.isNotEmpty)
+          .toList();
+      for (var processPath in processPaths) {
+        debugPrint(processPath);
+        // 在这里可以对进程路径进行进一步处理
+        //将这种 msedge.exe                        C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe                                                        22132
+        //拆分出来作为一个对象
+        //然后将这些对象放到一个数组里面
+        //最后将这个数组返回
+        ProcessInfo processInfo = ProcessInfo();
+        processInfo.processId =
+            processPath.substring(processPath.lastIndexOf(' ')).trim();
+        processInfo.processName =
+            processPath.substring(0, processPath.indexOf(' ')).trim();
+        processInfo.processPath = processPath.substring(
+            processPath.indexOf(' '), processPath.lastIndexOf(' ')).trim();
+        //json debug
+        // debugPrint(processInfo.toJson().toString());
+        ret.add(processInfo);
+      }
+    } else {
+      var error = processResult.stderr;
+      debugPrint('Command execution failed with error: $error');
+    }
+  } catch (e) {
+    debugPrint('Error executing command: $e');
+  }
+  return ret;
+}
+
+// ///使用Wmic命令获取服务信息
+// ///wmic service where (state=”running”) get caption, name, startmode
+// Future<List<ProcessInfo>> getServicesByWmic() async {
+//   List<ProcessInfo> ret = [];
+//   try {
+//     var processResult = await Process.run(
+//         'wmic', ['service', 'where', '(state="running")', 'get', 'caption,name,startmode']);
+//     if (processResult.exitCode == 0) {
+//       var output = processResult.stdout;
+//       var lines = LineSplitter.split(output);
+//       var processPaths = lines.skip(1)
+//           .where((line) => line.isNotEmpty)
+//           .toList();
+//       for (var processPath in processPaths) {
+//         debugPrint(processPath);
+//         // 在这里可以对进程路径进行进一步处理
+//         //将这种 msedge.exe                        C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe                                                        22132
+//         //拆分出来作为一个对象
+//         //然后将这些对象放到一个数组里面
+//         //最后将这个数组返回
+//         ProcessInfo processInfo = ProcessInfo();
+//         processInfo.processId =
+//             processPath.substring(processPath.lastIndexOf(' ')).trim();
+//         processInfo.processName =
+//             processPath.substring(0, processPath.indexOf(' ')).trim();
+//         processInfo.processPath = processPath.substring(
+//             processPath.indexOf(' '), processPath.lastIndexOf(' ')).trim();
+//         //json debug
+//         // debugPrint(processInfo.toJson().toString());
+//         ret.add(processInfo);
+//       }
+//     } else {
+//       var error = processResult.stderr;
+//       debugPrint('Command execution failed with error: $error');
+//     }
+//   } catch (e) {
+//     debugPrint('Error executing command: $e');
+//   }
+//   return ret;
+// }
